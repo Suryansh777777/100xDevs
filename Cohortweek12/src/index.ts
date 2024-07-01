786e13fa866808955309ae056447b49524c75a18
Suryansh Chourasia <suryanshchourasia12@gmail.com>
2024-07-01 22:35:26 +0530

    UnderStanding readonly syntax

--- Cohortweek12/src/index.ts
+++ Cohortweek12/src/index.ts
@@ -1,20 +1,47 @@
-interface User {
-    id: number;
-    name: string;
-    email: string;
-    createdAt: Date;
-  }
+// interface User {
+//     id: number;
+//     name: string;
+//     email: string;
+//     createdAt: Date;
+//   }
   
-  // For a profile display, only pick `name` and `email`
-  type UserProfile = Pick<User, 'name' | 'email'>;
+//   // For a profile display, only pick `name` and `email`
+//   type UserProfile = Pick<User, 'name' | 'email'>;
   
-  type UserProfilePartial = Partial<UserProfile>;
+//   type UserProfilePartial = Partial<UserProfile>;
 
-  const displayUserProfile = (user: UserProfilePartial) => {
-    console.log(`Name: ${user.name}, Email: ${user.email}`);
-  };
+//   const displayUserProfile = (user: UserProfilePartial) => {
+//     console.log(`Name: ${user.name}, Email: ${user.email}`);
+//   };
   
-  displayUserProfile({
+//   displayUserProfile({
+//     name: 'John Doe',
+//     email: 'johnde@asda'
+//     });
+
+//One way
+type User = {
+    readonly id: number;
+    readonly name: string;
+    readonly email: string;
+}
+
+const user: User = {
+    id: 1,
     name: 'John Doe',
-    email: 'johnde@asda'
-    });
+    email: 'johndoe@asda'
+}
+
+//Another way
+ 
+type Company = {
+     id: number;
+     name: string;
+     email: string;
+}
+
+const company: Readonly<Company> = {
+    id: 1,
+    name: 'John Doe',
+    email: 'johndoe@asda'
+}
