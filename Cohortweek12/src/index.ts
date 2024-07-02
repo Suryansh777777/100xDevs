a37cb5ad6569e959a0ea7b4a9c852c802e71b533
Suryansh Chourasia <suryanshchourasia12@gmail.com>
2024-07-02 09:51:37 +0530

    Type inference in zod

--- Cohortweek12/src/index.ts
+++ Cohortweek12/src/index.ts
@@ -85,11 +85,39 @@
 //   // Accessing a value using .get
 //   console.log(usersMap.get('abc123')); // Output: { id: 'abc123', name: 'John Doe' }
 
-type EventType = 'click' | 'scroll' | 'mousemove';
-type ExcludeEvent = Exclude<EventType, 'scroll'>; // 'click' | 'mousemove'
+// type EventType = 'click' | 'scroll' | 'mousemove';
+// type ExcludeEvent = Exclude<EventType, 'scroll'>; // 'click' | 'mousemove'
 
-const handleEvent = (event: ExcludeEvent) => {
-  console.log(`Handling event: ${event}`);
-};
+// const handleEvent = (event: ExcludeEvent) => {
+//   console.log(`Handling event: ${event}`);
+// };
 
-handleEvent('click'); // OK
+// handleEvent('click'); // OK
+
+import { z } from 'zod';
+import express from "express";
+
+const app = express();
+
+// Define the schema for profile update
+const userProfileSchema = z.object({
+  name: z.string().min(1, { message: "Name cannot be empty" }),
+  email: z.string().email({ message: "Invalid email format" }),
+  age: z.number().min(18, { message: "You must be at least 18 years old" }).optional(),
+});
+
+app.put("/user", (req, res) => {
+  const { success } = userProfileSchema.safeParse(req.body);
+  const updateBody = req.body; // how to assign a type to updateBody?
+
+  if (!success) {
+    res.status(411).json({});
+    return
+  }
+  // update database here
+  res.json({
+    message: "User updated"
+  })
+});
+
+app.listen(3000);
