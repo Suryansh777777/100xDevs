533ee7d27fc8f7748a2e7f5ca33bfeac18cb3d5a
Suryansh Chourasia <suryanshchourasia12@gmail.com>
2024-07-12 01:06:08 +0530

    Understanding user route to store new user in database

--- Cohortweek14/week-14.2/app/api/user/route.ts
+++ Cohortweek14/week-14.2/app/api/user/route.ts
@@ -1,9 +1,16 @@
 import { NextRequest } from "next/server";
-
+import { PrismaClient } from "@prisma/client";
+const client = new PrismaClient();
 export async function POST(req: NextRequest){
     //extract the body and store in db
     const body = await req.json();
-    console.log(body)
+    await client.user.create({
+        data:{
+            username:body.username,
+            password:body.password
+        }
+    })
+    
     return Response.json({
         message:"You are logged in the account"
     })
